571298cbce20764bf09fb9154a5fc04d44bb2227
Juha Ylitalo <juha.ylitalo@reaktor.com>
2024-07-06 22:29:02 +0300

    [server/best] Add activities total time to best efforts

--- pkg/stats/best.go
+++ pkg/stats/best.go
@@ -10,7 +10,7 @@ func Best(db Storage, distance string, limit int) ([]string, [][]string, error) 
 	o := []string{"besteffort.movingtime", "year", "month", "day"}
 	rows, err := db.QueryBestEffort(
 		[]string{
-			"year", "month", "day", "summary.name", "summary.distance",
+			"year", "month", "day", "summary.name", "summary.distance", "summary.elapsedtime",
 			"besteffort.movingtime", "besteffort.elapsedtime",
 			"summary.StravaID",
 		},
@@ -22,10 +22,10 @@ func Best(db Storage, distance string, limit int) ([]string, [][]string, error) 
 	defer rows.Close()
 	results := [][]string{}
 	for rows.Next() {
-		var year, month, day, movingTime, elapsedTime, stravaID int
+		var year, month, day, movingTime, elapsedTime, totalTime, stravaID int
 		var distance float64
 		var name string
-		err = rows.Scan(&year, &month, &day, &name, &distance, &movingTime, &elapsedTime, &stravaID)
+		err = rows.Scan(&year, &month, &day, &name, &distance, &totalTime, &movingTime, &elapsedTime, &stravaID)
 		if err != nil {
 			return nil, nil, err
 		}
@@ -33,8 +33,9 @@ func Best(db Storage, distance string, limit int) ([]string, [][]string, error) 
 			fmt.Sprintf("%2d.%2d.%d", day, month, year), name,
 			fmt.Sprintf("%2d:%02d:%02d", elapsedTime/3600, elapsedTime/60%60, elapsedTime%60),
 			fmt.Sprintf("%.2f", distance/1000),
+			fmt.Sprintf("%2d:%02d:%02d", totalTime/3600, totalTime/60%60, totalTime%60),
 			fmt.Sprintf("https://strava.com/activities/%d", stravaID),
 		})
 	}
-	return []string{"Date", distance, "Time", "Total (km)", "Link"}, results, nil
+	return []string{"Date", distance, "Time", "Total (km)", "Total (time)", "Link"}, results, nil
 }
